93e42bed3bd6d1daf7f284530ff3434cb0187111
joaopdantas <joao.dantas@ipvc.pt>
2024-02-01 22:23:36 +0000

    production work

--- src/main/java/com/salinas/salinasdovouga/Controllers/ProductionManagerController.java
+++ src/main/java/com/salinas/salinasdovouga/Controllers/ProductionManagerController.java
@@ -1,37 +1,130 @@
 package com.salinas.salinasdovouga.Controllers;
 
-import com.salinas.salinasdovouga.Model.ProductionLot;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ListView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import com.salinas.salinasdovouga.Model.ProductionLot;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class ProductionManagerController {
 
     @FXML
-    private ListView<ProductionLot> lotListView;
-
-    private ObservableList<ProductionLot> productionLots;
+    private TextField lotNumberField;
 
     @FXML
-    private void initialize() {
-        // Initialize production lots (this can be loaded from a data source)
-        productionLots = FXCollections.observableArrayList();
-        // Set the items to the lotListView
-        lotListView.setItems(productionLots);
-    }
+    private DatePicker productionDatePicker;
+
+    @FXML
+    private TextField tanksField;
+
+    @FXML
+    private TextField workersField;
+
+    @FXML
+    private ChoiceBox<String> productTypeChoiceBox;
+
+    @FXML
+    private TextField weightQuantityField;
+
+    @FXML
+    private TableView<ProductionLot> productionTableView;
+
+    @FXML
+    private TableColumn<ProductionLot, String> lotNumberColumn;
+    @FXML
+    private TableColumn<ProductionLot, LocalDate> productionDateColumn;
+    @FXML
+    private TableColumn<ProductionLot, String> associatedTanksColumn;
+    @FXML
+    private TableColumn<ProductionLot, String> associatedWorkersColumn;
+    @FXML
+    private TableColumn<ProductionLot, String> productTypeColumn;
+    @FXML
+    private TableColumn<ProductionLot, String> weightQuantityColumn;
 
     @FXML
     private void handleCreateNewBatch() {
-        // Create a new production lot
-        ProductionLot newLot = new ProductionLot();
+        try {
+            // Load the registration.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/salinas/salinasdovouga/Production Actions/create_new_batch.fxml"));
+            Parent root = loader.load();
 
-        // Add the new lot to the list
-        productionLots.add(newLot);
+            if (loader.getController() == null) {
+                System.out.println("Controller is null");
+            }
 
-        showAlert("Create New Batch", "New batch created successfully.");
+            // Create a new stage for the registration window
+            Stage createNewBatchStage = new Stage();
+            createNewBatchStage.setTitle("Create New Batch");
+            createNewBatchStage.setScene(new Scene(root));
+
+            // Show the registration window
+            createNewBatchStage.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert("Error", "Unable to load create new batch window.");
+        }
     }
+
+    @FXML
+    private void initialize() {
+        // Set up the cell value factories for each column
+        lotNumberColumn.setCellValueFactory(new PropertyValueFactory<>("lotNumber"));
+        productionDateColumn.setCellValueFactory(new PropertyValueFactory<>("productionDate"));
+        associatedTanksColumn.setCellValueFactory(new PropertyValueFactory<>("associatedTanks"));
+        associatedWorkersColumn.setCellValueFactory(new PropertyValueFactory<>("associatedWorkers"));
+        productTypeColumn.setCellValueFactory(new PropertyValueFactory<>("productType"));
+        weightQuantityColumn.setCellValueFactory(new PropertyValueFactory<>("weightQuantity"));
+
+        // Refresh the table view with current production lot data
+        refreshProductionTableView();
+    }
+    @FXML
+    private void refreshProductionTableView() {
+        // Clear existing items
+        productionTableView.getItems().clear();
+
+        // Load all production lots and add them to the TableView
+        productionTableView.getItems().addAll();
+
+    }
+
+   /* @FXML
+    private void handleCreateNewBatchADD() {
+        try {
+            // Retrieve information from the FXML fields
+            int lotNumber = Integer.parseInt(lotNumberField.getText());
+            LocalDate productionDate = productionDatePicker.getValue().atStartOfDay().toLocalDate();
+            String tanks = tanksField.getText();
+            String workers = workersField.getText();
+            String productType = productTypeChoiceBox.getValue();
+            double weightQuantity = Double.parseDouble(weightQuantityField.getText());
+
+
+            // Create a new ProductionLot
+            ProductionLot newProductionLot = new ProductionLot(lotNumber, productionDate, tanks, workers, productType, weightQuantity);
+            newProductionLot.setAssociatedTanks(tanks);
+            newProductionLot.setAssociatedWorkers(workers);
+            newProductionLot.setProductType(productType);
+            newProductionLot.setWeightQuantity(weightQuantity);
+
+            // TODO: Add code to save the newProductionLot or perform any other necessary actions
+
+            showAlert("Batch Created", "New batch created successfully!");
+
+        } catch (NumberFormatException | NullPointerException e) {
+            showAlert("Error", "Please fill in all fields with valid data.");
+            e.printStackTrace();
+        }
+    }*/
 
     private void showAlert(String title, String content) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
--- src/main/java/com/salinas/salinasdovouga/Model/ProductionLot.java
+++ src/main/java/com/salinas/salinasdovouga/Model/ProductionLot.java
@@ -1,24 +1,40 @@
 package com.salinas.salinasdovouga.Model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
 
 public class ProductionLot implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private int lotNumber;
-    private String productionDate;
+    private LocalDate productionDate;
+    private String associatedTanks;
+    private String associatedWorkers;
+    private String productType;
+    private Double weightQuantity;
 
     // Constructors, getters, setters, etc.
+
+
+
 
     public ProductionLot() {
         // Default constructor
     }
 
-    public ProductionLot(int lotNumber, String productionDate) {
+    public ProductionLot(int lotNumber, LocalDate productionDate, String associatedTanks,
+                         String associatedWorkers, String productType, Double weightQuantity) {
         this.lotNumber = lotNumber;
         this.productionDate = productionDate;
+        this.associatedTanks = associatedTanks;
+        this.associatedWorkers = associatedWorkers;
+        this.productType = productType;
+        this.weightQuantity = weightQuantity;
     }
+
+
 
     public int getLotNumber() {
         return lotNumber;
@@ -28,19 +44,70 @@ public class ProductionLot implements Serializable {
         this.lotNumber = lotNumber;
     }
 
-    public String getProductionDate() {
+    public LocalDate getProductionDate() {
         return productionDate;
     }
 
-    public void setProductionDate(String productionDate) {
+    public void setProductionDate(LocalDate productionDate) {
         this.productionDate = productionDate;
+    }
+
+    public String getAssociatedTanks() {
+        return associatedTanks;
+    }
+
+    public void setAssociatedTanks(String associatedTanks) {
+        this.associatedTanks = associatedTanks;
+    }
+
+    public String getAssociatedWorkers() {
+        return associatedWorkers;
+    }
+
+    public void setAssociatedWorkers(String associatedWorkers) {
+        this.associatedWorkers = associatedWorkers;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public Double getWeightQuantity() {
+        return (weightQuantity != null) ? weightQuantity.doubleValue() : 0.0;
+    }
+
+
+    public void setWeightQuantity(double weightQuantity) {
+        this.weightQuantity = weightQuantity;
     }
 
     @Override
     public String toString() {
         return "ProductionLot{" +
-                "lotNumber=" + lotNumber +
-                ", productionDate='" + productionDate + '\'' +
+                "lotNumber='" + lotNumber + '\'' +
+                ", productionDate=" + productionDate +
+                ", associatedTanks='" + associatedTanks + '\'' +
+                ", associatedWorkers='" + associatedWorkers + '\'' +
+                ", productType='" + productType + '\'' +
+                ", weightQuantity='" + weightQuantity + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductionLot that = (ProductionLot) o;
+        return lotNumber == that.lotNumber;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotNumber);
     }
 }
